d913ffe3df326ba20c0b61d1411f1b9e370054a1
Hubert Rebialkowski <hubertrebialkowski@gmail.com>
2018-05-17 19:46:37 +0200

    Inicjalizacja

---
+++ Logika.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cezar
+{
+    class Logika
+    {
+        char[] Litery = { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ż', 'ź' };
+
+        public string szyfruj(String wiadomosc, int przesuniecie)
+        {
+            String szyfr = "";
+            wiadomosc = wiadomosc.ToLower();
+            char[] znaki = wiadomosc.ToCharArray();
+
+            for(int i=0; i<znaki.Length; i++)
+            {
+            	bool spojniki=false;
+            	
+                for(int j=0; j<Litery.Length; j++)
+                {                		
+                    if (znaki[i].Equals(Litery[j]))
+                    {
+                    	int x=j + przesuniecie;
+                    	if(x>=Litery.Length)
+                    		x=przesuniecie-(Litery.Length-j);
+                    		
+                        char znak = Litery[x];
+                        szyfr += znak;
+                        spojniki=true;
+                    }
+                }
+                if(spojniki==false)
+                	szyfr+=znaki[i];
+            }
+
+            return szyfr;
+        }
+        
+        public string deszyfruj(String szyfr, int przesuniecie)
+        {
+            String wiadomosc = "";
+            szyfr = szyfr.ToLower();
+            char[] znaki = szyfr.ToCharArray();
+
+            for(int i=0; i<znaki.Length; i++)
+            {
+            	bool spojniki=false;
+            	
+                for(int j=0; j<Litery.Length; j++)
+                {                		
+                    if (znaki[i].Equals(Litery[j]))
+                    {
+                    	int x=j - przesuniecie;
+                    	if(x<0)
+                    		x=Litery.Length-przesuniecie+j;
+                    		
+                        char znak = Litery[x];
+                        wiadomosc += znak;
+                        spojniki=true;
+                    }
+                }
+                if(spojniki==false)
+                	wiadomosc+=znaki[i];
+            }
+
+            return wiadomosc;
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cezar
+{
+    public partial class Form1 : Form
+    {
+        Logika l;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            l = new Cezar.Logika();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String tuser = textBox1.Text;
+            String tprzes = textBox3.Text;
+            int przesuniecie=0;
+            bool num=int.TryParse(tprzes, out przesuniecie);
+
+            if(num==true && przesuniecie>0)
+            {
+            	String szyfr = l.szyfruj(tuser, przesuniecie);
+            	textBox5.Text = szyfr;
+            }
+            else
+            	textBox5.Text="Przesunięcie musi być liczbą większą od zera.";
+        }
+        
+		void Button2Click(object sender, EventArgs e)
+		{
+			String tuser = textBox2.Text;
+            String tprzes = textBox4.Text;
+            int przesuniecie=0;
+            bool num=int.TryParse(tprzes, out przesuniecie);
+
+            if(num==true && przesuniecie>0)
+            {
+            	String wiadomosc = l.deszyfruj(tuser, przesuniecie);
+            	textBox6.Text = wiadomosc;
+            }
+            else
+            	textBox6.Text="Przesunięcie musi być liczbą większą od zera.";
+		}
+    }
+}
